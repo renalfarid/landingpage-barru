16384bfbd14806656942ed79bc1584b9708297fb
renalfarid <enal.farid@gmail.com>
2024-09-25 18:13:18 +0800

    build tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './*/*.html',
+    './assets/js/*.js',
+    './assets/blog/js/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
